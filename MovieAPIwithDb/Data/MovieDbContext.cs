0f60636c2d61a3e46bae832388832bdf905d0a35
HP <HP@DESKTOP-AQ07QGH>
2024-02-07 15:51:49 +0530

    Add project files.

---
+++ MovieAPIwithDb/Data/MovieDbContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using MovieAPIwithDb.Models;
+
+namespace MovieAPIwithDb.Data
+{
+    public class MovieDbContext : DbContext
+    {
+        public MovieDbContext (DbContextOptions<MovieDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<MovieAPIwithDb.Models.Movie> Movie { get; set; } = default!;
+    }
+}
---
+++ MovieAPIwithDb/Models/Movie.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MovieAPIwithDb.Models
+{
+    [Table("Movies")]
+    public class Movie
+    {
+        [Key]
+        public int Id { get; set; }
+        [StringLength(100)]
+        public string? MName { get; set; }
+        [StringLength(100)]
+        public string? StarCast { get; set; }
+        [StringLength(100)]
+        public string? Director { get; set; }
+        [StringLength(100)]
+        public string? Producer { get; set; }
+        public DateTime ReleaseDate { get; set; }
+    }
+}
